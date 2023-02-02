fac409419eeb30257af95eccb48723132a0d841e
ibtihel <sibtyhel@gmail.com>
2023-02-03 00:48:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import photo from "./profil.jpg";
+import Profile from "./profile/Profile";
+function App() {
+  const profile = {
+    fullName: "SAIDI Ibtihel",
+    bio: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    profession: "Student",
+    pic: photo,
+  };
+  return (
+    <div className="App">
+      <Profile
+        fullName={profile.fullName}
+        bio={profile.bio}
+        profession={profile.profession}
+      >
+        <img src={profile.pic} alt="" srcset="" />
+      </Profile>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/profile/Profile.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import PropTypes from "prop-types";
+const Profile = (props) => {
+  const { fullName, bio, profession, children } = props;
+  const showfullName = (fullName) => alert(`full name: ${fullName}`);
+  const handleClick = (e) => showfullName(fullName);
+  return (
+    <div
+      className="card"
+      style={{
+        background: "#FF7F50",
+        maxWidth: " 600px",
+        margin: "auto",
+        textalign: "center",
+      }}
+    >
+      <h2
+        style={{
+          color: "#F0E68C",
+          textAlign: "center",
+        }}
+      >
+        Name
+      </h2>
+      <h4>{fullName}</h4>
+      <h2
+        style={{
+          color: "#F0E68C",
+          textAlign: "center",
+        }}
+      >
+        Profession
+      </h2>
+      <h4> {profession} </h4>
+      <h2
+        style={{
+          color: "#F0E68C",
+          textAlign: "center",
+        }}
+      >
+        Biography
+      </h2>
+      <p> {bio}</p>
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        {children}
+      </div>
+      <br />
+      <button>
+        <a href="#!" onClick={handleClick} style={{ color: "#A0522D" }}>
+          Full Name
+        </a>
+      </button>
+    </div>
+  );
+};
+Profile.defaultProps = {
+  fullName: "default fullName",
+  profession: "default profession",
+  bio: " default bio",
+};
+Profile.propTypes = { profil: PropTypes.string.isRequired };
+
+export default Profile;
